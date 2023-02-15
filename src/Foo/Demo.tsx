4a937cc8c9f5601ef214df16a119de8c0b0a4a8d
enny.wang <enny.wang@molimediagroup.com>
2023-02-15 13:55:37 +0800

    chore: dumi初始化

---
+++ src/Foo/Demo.tsx
@@ -0,0 +1,4 @@
+import { Foo } from 'dumi-demo';
+import React from 'react';
+
+export default () => <Foo title="Hello dumi! Foo" />
---
+++ src/Foo/index.md
@@ -0,0 +1,6 @@
+# Foo
+
+This is an example component.
+
+<code src="@/Foo2/Demo.tsx"></code>
+<code src="@/Foo2/Demo.tsx"></code>
